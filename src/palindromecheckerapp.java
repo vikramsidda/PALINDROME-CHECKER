7177539883176ff97f4589d614fff937a328974f
vikramsidda <vikramsidda2007@gmail.com>
2026-02-25 11:03:42 +0530

    UC1:Welcome message

---
+++ src/palindromecheckerapp.java
@@ -0,0 +1,21 @@
+public class palindromecheckerapp {
+    static final String APP_NAME = "Palindrome Checker App";
+    static final String VERSION = "Version 1.0";
+
+    public static void main(String[] args) {
+
+        // Welcome Message
+        System.out.println("======================================");
+        System.out.println("        " + APP_NAME);
+        System.out.println("        " + VERSION);
+        System.out.println("======================================");
+
+        System.out.println("Welcome to the Palindrome Checker Application!");
+        System.out.println("This application checks whether a given string is a palindrome.");
+        System.out.println("--------------------------------------");
+
+        // Program continues to next use case
+        System.out.println("System Ready...");
+    }
+}
+
